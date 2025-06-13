7e1c892b73c2c4a96dfcf6f573e25f3bc38ea4e3
Isa <isabel.mir687@gmail.com>
2025-06-12 23:47:07 -0300

    Correciones

--- script.js
+++ script.js
@@ -7,3 +7,45 @@ menu.addEventListener("click",()=>{
     menu.classList.toggle("menu-toggle");
     main.classList.toggle("menu-toggle");
 });
+// Seleccionamos las clases "counter"
+const counters = document.querySelectorAll('.counter');
+// Recorremos cada contador 
+counters.forEach(counter => {
+    // Obtenemos el número final de data-target 
+    const target = +counter.getAttribute('data-target');
+    // tipo de formato que debe usar (money o none)
+    const format = counter.getAttribute('data-format') || 'none';
+    // contador cero
+    let count = 0;
+    // Tiempo de la animación (ml)
+    const duration = 2000;
+    // pasos en los que se va a dividir la animación
+    const steps = 100;
+    // Cuánto debe aumentar en cada paso
+    const increment = target / steps;
+    // espera entre cada paso
+    const delay = duration / steps;
+    // actualiza el número mostrado
+    const updateCounter = () => {
+        // Aumentamos el contador 
+        count += increment;
+        // le agregamos el signo $ y puntos
+        if (format === 'money') {
+            displayValue = `$${Math.floor(count).toLocaleString('es-AR')}`;
+        } else {
+            //  solo mostramos el número normal
+            displayValue = `${Math.floor(count)}`;
+        }
+
+        // Mostramos el valor en el HTML
+        counter.innerText = displayValue;
+
+        // Si todavía no llegamos al número final, volvemos a llamar a esta función
+        if (count < target) {
+            setTimeout(updateCounter, delay);
+        }
+    };
+
+    // Empezamos la animación para este contador
+    updateCounter();
+});
